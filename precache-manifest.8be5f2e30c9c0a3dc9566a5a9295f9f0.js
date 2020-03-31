bc5bfbad7c652873f2a2bf743503b8bab87750fe
Sergei Vassiljev <sergei.vassiljev98@gmail.com>
2020-03-31 12:59:42 +0300

    Updates

---
+++ precache-manifest.8be5f2e30c9c0a3dc9566a5a9295f9f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6abfc54815ad2cdb9120317c94d4602",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "6c2168ebdf0137592148",
+    "url": "/ToDo-App/static/css/main.80ef8a39.chunk.css"
+  },
+  {
+    "revision": "8a4c9327b76b1bc8219e",
+    "url": "/ToDo-App/static/js/2.c39d8661.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/ToDo-App/static/js/2.c39d8661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2168ebdf0137592148",
+    "url": "/ToDo-App/static/js/main.5d52d9fb.chunk.js"
+  },
+  {
+    "revision": "3f2d7047c1070fbe1c6c",
+    "url": "/ToDo-App/static/js/runtime-main.cef2f909.js"
+  },
+  {
+    "revision": "04bce1bad57c42f2363ab204267bb4eb",
+    "url": "/ToDo-App/static/media/add.04bce1ba.svg"
+  },
+  {
+    "revision": "cf3d8dbc0d04e59f0c64ee376d47308f",
+    "url": "/ToDo-App/static/media/close.cf3d8dbc.svg"
+  },
+  {
+    "revision": "e6d3868a10f056f22a226b8cbe602bc9",
+    "url": "/ToDo-App/static/media/edit.e6d3868a.svg"
+  },
+  {
+    "revision": "95f30ee9e97d62b2666b1f9c48e00ae7",
+    "url": "/ToDo-App/static/media/remove.95f30ee9.svg"
+  }
+]);
--- src/components/Badge/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './Badge.scss';
-import classNames from 'classnames'
-
-const Badge = ({ color, onClick, className }) => (
-    <i onClick={onClick} className={classNames('badge', { [`badge--${color}`]: color }, className)} />
-);
-
-export default Badge;
